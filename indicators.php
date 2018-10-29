307fd3200052f2e3462e3bf8457d7a58f73114eb
Michael Barnicle <michaelbarnicle@Michaels-MacBook-Pro-2.local>
2018-10-28 22:27:10 -0500

    update indicators.php

--- indicators.php
+++ indicators.php
@@ -84,103 +84,101 @@
                 <span class="hamb-bottom"></span>
             </button>
             <div class="container">
-
-
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-    </ol>
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                    </ol>
 
     <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-1">
-                        <h3 style="font-size: 30px;">10%</h3>
-                        <p>Children who are "school-ready" when they arrive at Kindergarten</p>
-                        <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
+                    <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-md-offset-1">
+                                <h3 style="font-size: 30px;">10%</h3>
+                                <p>Children who are "school-ready" when they arrive at Kindergarten</p>
+                                <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
+                            </div>
+                            <div class="col-md-7">
+                                <div id="container1"></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-7">
-                        <div id="container1"></div>
+              </div>
+              <div class="item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-md-offset-1">
+                                <h3 style="font-size: 30px;">10%</h3>
+                                <p>Children who are "school-ready" when they arrive at Kindergarten</p>
+                                <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
+                            </div>
+                            <div class="col-md-7">
+                                <div id="container2" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-      </div>
-      <div class="item">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-1">
-                        <h3 style="font-size: 30px;">10%</h3>
-                        <p>Children who are "school-ready" when they arrive at Kindergarten</p>
-                        <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
+              </div>
+              <div class="item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-md-offset-1">
+                                <h3 style="font-size: 30px;">10%</h3>
+                                <p>Children who are "school-ready" when they arrive at Kindergarten</p>
+                                <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
+                            </div>
+                            <div class="col-md-7">
+                                <div id="container3"></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-7">
-                        <div id="container2" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+              </div>
+              <div class="item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-md-offset-1">
+                                <h3 style="font-size: 30px;">10%</h3>
+                                <p>Children who are "school-ready" when they arrive at Kindergarten</p>
+                                <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
+                            </div>
+                            <div class="col-md-7">
+                                <div id="container4"></div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-      </div>
-      <div class="item">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-1">
-                        <h3 style="font-size: 30px;">10%</h3>
-                        <p>Children who are "school-ready" when they arrive at Kindergarten</p>
-                        <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
+              </div>
+              <div class="item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-md-offset-1">
+                                <h3 style="font-size: 30px;">10%</h3>
+                                <p>Children who are "school-ready" when they arrive at Kindergarten</p>
+                                <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
+                            </div>
+                            <div class="col-md-7">
+                                <div id="container5"></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-7">
-                        <div id="container3"></div>
+              </div>
                     </div>
-                </div>
-            </div>
-      </div>
-      <div class="item">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-1">
-                        <h3 style="font-size: 30px;">10%</h3>
-                        <p>Children who are "school-ready" when they arrive at Kindergarten</p>
-                        <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
-                    </div>
-                    <div class="col-md-7">
-                        <div id="container4"></div>
-                    </div>
-                </div>
-            </div>
-      </div>
-      <div class="item">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-1">
-                        <h3 style="font-size: 30px;">10%</h3>
-                        <p>Children who are "school-ready" when they arrive at Kindergarten</p>
-                        <p>The mission of the North Chicago Project is to increase, substantially, the percentage of North Chicago children who will earn a quality post-secondary credential.</p>
-                    </div>
-                    <div class="col-md-7">
-                        <div id="container5"></div>
-                    </div>
-                </div>
-            </div>
-      </div>
-    </div>
 
     <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
 
+                </div>
             </div>
         </div>
         <!-- /#page-content-wrapper -->
